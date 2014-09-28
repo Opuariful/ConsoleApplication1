89c35e58de182b473a37bbec52d2a8085507aa5e
Opuariful <oopu.ariful@gmail.com>
2014-09-28 20:55:24 +0600

    This is my first console application.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 65;
+            char character = Convert.ToChar(number);
+            Console.WriteLine(character);
+            Console.ReadKey();
+        }
+    }
+}
